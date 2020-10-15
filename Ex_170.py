4397d78cf3f7808c04ff8399e1e92ba2505588e5
Inimesh <72313368+Inimesh@users.noreply.github.com>
2020-10-15 16:08:34 +0100

    file organisation and completed files

--- Ex_170.py
+++
@@ -1,7 +0,0 @@
-## A recursive function that determines wheter or not it is possible to
-# construct a particular total using a specific number of coins. e.g. can you
-# have a total of $1.00 using four coins? Yes, if each coin is a quarter,
-# however you cannot have $1.00 using five coins. Read input from the user and
-# display a message indicating whether or not it is possible. ##
-
-# I give up. Long time spent on this. No solutions. See Learning notes
